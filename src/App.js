c7c90db6b181a76fb86d977f94c86bf3d508a3a7
fahad1205 <fahadbravo5@gmail.com>
2023-03-06 23:40:56 +0530

    Second day React Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+export function Component1(){
+return(
+  <h1>I am heading one...from first component</h1>
+)
+}
+
+export function Component2(){
+return(
+  <h2>I am heading two...from second component</h2>
+)
+}
+
+export function Component3(){
+return(
+  <h3>I am heading three...from third component</h3>
+)
+}
+
+export function Component4(){
+return(
+  <button >Click me 1</button>
+)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <Component1/>
+    <Component2/>
+    <Component3/>
+    <Component4/> */}
+      {/* <fragment>
+      <h1>I am heading one...from first component</h1>
+      <h2>I am heading two...from second component</h2>
+      <h3>I am heading three...from third component</h3>
+      <button >Click me 1</button>
+      </fragment> */}
     </div>
   );
 }
